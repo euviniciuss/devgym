6a2ecc95f3b082f0151f7c549171a912c757a7e1
euviniciuss <dev.mvsc@gmail.com>
2026-04-12 15:39:43 -0300

    feat: adicionando prisma orm e criando tabelas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
+import { PrismaPg } from '@prisma/adapter-pg'
 import Fastify from 'fastify'
+import { env } from '@/env'
+import { PrismaClient } from '../generated/prisma/client'
 
 export const app = Fastify({
 	logger: false,
+})
+
+export const schema =
+	new URL(env.DATABASE_URL).searchParams.get('schema') || 'public'
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL }, { schema })
+
+const prisma = new PrismaClient({
+	adapter,
+	log: env.NODE_ENV === 'dev' ? ['query'] : [],
+})
+
+prisma.user.create({
+	data: {
+		name: 'Marcus Vinicius',
+		email: 'teste@gmail.com',
+	},
 })
