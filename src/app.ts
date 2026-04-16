a5e142737b5182f19174b575e57f9b347874ba7b
euviniciuss <dev.mvsc@gmail.com>
2026-04-16 16:20:41 -0300

    feat: adicionando tratativa global de errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
-
+import { ZodError, z } from 'zod'
+import { env } from './env'
 import { appRoutes } from './http/routes'
 
 export const app = Fastify({
@@ -7,3 +8,20 @@ export const app = Fastify({
 })
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _request, reply) => {
+	if (error instanceof ZodError) {
+		return reply.status(400).send({
+			message: 'Validation error',
+			issues: z.treeifyError(error),
+		})
+	}
+
+	if (env.NODE_ENV !== 'production') {
+		console.error(error)
+	}
+
+	return reply.status(500).send({
+		message: 'Internal server error',
+	})
+})
